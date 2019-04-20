f42524db88e68821e7c1c1ebd9e0eb23d3e41d15
wei feng <theweifeng@gmail.com>
2019-04-20 01:51:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def apply_coupons(cart, coupons)
       ele.each do |coupon_item, coupon_values|
         hash[item] = values
         if item == ele[:item]
-          hash[item + " W/COUPON"] = {:price => ele[:cost], :clearance => values[:clearance], :count => coupons.count(item)}
+          hash[item + " W/COUPON"] = {:price => ele[:cost], :clearance => values[:clearance], :count => coupons.count(coupon_item)}
         end
       end
           hash[item][:count] -= ele[:num] if item == ele[:item]
