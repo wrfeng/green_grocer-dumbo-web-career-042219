5622bed7b558802d21c34914400e437991c6653f
wei feng <theweifeng@gmail.com>
2019-04-20 02:43:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ def checkout(cart, coupons)
   
   consolidated_cart = consolidate_cart(cart)
   # binding.pry
-  couponed_cart = apply_coupons(consolidated_cartcart, coupons)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
     # binding.pry
   clearance_cart = apply_clearance(couponed_cart)
     # binding.pry
