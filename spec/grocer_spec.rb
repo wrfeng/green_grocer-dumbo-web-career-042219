0fbe4f468f2ad223f1c304bb06a2add49af429ff
wei feng <theweifeng@gmail.com>
2019-04-20 01:01:14 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -58,8 +58,6 @@ describe "Grocer" do
         @cart = [@avocado, @avocado]
         @consolidated_cart = consolidate_cart(@cart)
         @avocado_result = apply_coupons(@consolidated_cart, [@avocado_coupon])
-        
-        binding.pry
       end
 
       it "adds a new key, value pair to the cart hash called 'ITEM NAME W/COUPON'" do
